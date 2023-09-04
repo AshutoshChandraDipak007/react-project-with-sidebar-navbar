f776e4f1d439ddf460634eeef4a2a7d7f3cdf747
Dipak <ashutosh-chandra.dipak@capgemini.com>
2023-09-04 16:30:25 +0530

    changes for toggle button

--- src/components/parent/Parent.js
+++ src/components/parent/Parent.js
@@ -5,24 +5,15 @@ import './parent.css';
 
 function Parent({children}) {
   const flag=useSelector((state) => state.counter.isToggle);
-
-  const cssStyle={
-    margin:'60px 0px 0px 250px',
-    top: 0, 
-    position:'absolute',
-    border:'15px solid rgb(236, 240, 240)',
-  }
-
   return (
     <>   
     <div className='sidebar'>      
        <SideBar />
     </div>
-    <div className={`${flag?"parent-component-hide":"parent-component"}`}>
+    <div className={`${flag?"nav-open":"nav-close"}`}>
         {children}
     </div>
-    </>
-  
+    </>  
   )
 }
 
